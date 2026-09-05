502ae51f2f9e74fddf3135ca8476c82c588925d5
Alejandro Resendez <al.resendez.gro@gmail.com>
2026-09-05 16:35:16 -0700

    adding the function to check the FRU step by step

--- ssh_tester.py
+++ ssh_tester.py
@@ -181,25 +181,32 @@ def reset_c13_module(rm:SSHClientWrapper, logger:Logger):
     return not("Failure" in out)
 
 def check_c13_status(rm:SSHClientWrapper):
-    rm.query(C13_STATUS_ALL)
-    rm.query(C13_FRU)
-    rm.query(C13_READING_ALL)
+    s = rm.query(C13_STATUS_ALL)
+    print_w_ts(f"Status:\n{s}")
+    fru = rm.query(C13_FRU)
+    print_w_ts(f"FRU Content:\n{fru}")
+    r = rm.query(C13_READING_ALL)
+    print_w_ts(f"Reading\n{r}")
+    print_w_ts("Completed checking PSHELF Status")
     return
 
 def test_c13_modules(rm:SSHClientWrapper, logger:Logger):
 
     # Shut down all C13 modules
     for i in range(8):
+        print_w_ts(f"Turn off {i+1}")
         rm.query(f"set powershelf c13 off -c {i+1}")
     
     check_c13_status(rm)
-    c13_arrangement = [[1,2], [3,4], [5,6], [7,8]]
+    c13_arrangement = [[1,2, 3,4], [5,6, 7,8]]
 
     for c13 in c13_arrangement:
         logger.log("TEST_MODULE", f"Turning ON {c13}")
         print_w_ts(f"Turning on modules {c13}")
         for module in c13:
             rm.query(f"set powershelf c13 on -c {module}")
+        print_w_ts("Waiting for 2 min for C13 to Init...")
+        time.sleep(120)
         check_c13_status(rm)
         logger.log("TEST_MODULE", f"Turning OFF modules {c13}")
         for module in c13:
